8d29143455a778123b09a370c1bc5d5156be0565
anshulgaharwal <anshulgaharwal@gmail.com>
2025-12-29 00:32:38 +0530

    added analytics

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
+import { Line, Pie } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 export default function Home() {
 
@@ -26,6 +28,47 @@ export default function Home() {
       </p>
 
       <div className="overflow-x-auto">
+        {/* Charts Section */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+          {/* Attack Trend */}
+          <div className="bg-gray-900 p-4 rounded-lg">
+            <h2 className="text-xl font-semibold mb-2">Attack Trend</h2>
+            <Line
+              data={{
+                labels: data.map(x => x.time),
+                datasets: [
+                  {
+                    label: "Requests per Minute",
+                    data: data.map(x => x.requests_per_min),
+                    borderColor: "rgb(99, 179, 237)",
+                    borderWidth: 2
+                  }
+                ]
+              }}
+            />
+          </div>
+            {/* Severity Distribution */}
+            <div className="bg-gray-900 p-4 rounded-lg">
+              <h2 className="text-xl font-semibold mb-2">Severity Distribution</h2>
+
+              <Pie
+                data={{
+                  labels: ["CRITICAL", "MODERATE", "LOW"],
+                  datasets: [
+                    {
+                      data: [
+                        data.filter(a => a.severity === "CRITICAL").length,
+                        data.filter(a => a.severity === "MODERATE").length,
+                        data.filter(a => a.severity === "LOW").length
+                      ],
+                      backgroundColor: ["#dc2626", "#ca8a04", "#16a34a"]
+                    }
+                  ]
+                }}
+              />
+            </div>
+
+          </div>
         <table className="w-full border border-gray-700 rounded-lg">
           <thead className="bg-gray-800">
             <tr>
